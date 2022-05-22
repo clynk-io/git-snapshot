6a84677f1e262b158aa264a02f7caf4ff6d0b53d
asdf <asdf@asdg.com>
2022-05-21 23:08:17 -0700

    snapshot

--- src/repo.rs
+++ src/repo.rs
@@ -56,8 +56,8 @@ impl Repo {
 
             let mut callbacks = RemoteCallbacks::new();
             let mut config = self.git_repo.config()?;
-            config.add_file(&git2::Config::find_global()?, ConfigLevel::Global, true)?;
-            config.add_file(&git2::Config::find_system()?, ConfigLevel::System, true)?;
+            config.add_file(&git2::Config::find_global()?, ConfigLevel::Global, true);
+            config.add_file(&git2::Config::find_system()?, ConfigLevel::System, true);
 
             callbacks.credentials(move |url, username, allowed_types| {
                 Cred::credential_helper(&config, url, username)
