818bc253f0864d351344d24e3007e32fcfe7658d
asdf <asdf@asdg.com>
2022-05-22 17:50:55 -0700

    snapshot

--- src/repo.rs
+++ src/repo.rs
@@ -5,6 +5,7 @@ use git2::{
     Repository, Signature,
 };
 use std::path::Path;
+use std::sync::Arc;
 
 pub struct Repo {
     git_repo: Repository,
@@ -60,31 +61,44 @@ impl Repo {
                 .unwrap_or_default(),
         )?;
 
-        for remote in &self.config.remotes {
-            let mut remote = self.git_repo.find_remote(&remote.name)?;
+        self.push(&snapshot_ref)
+    }
 
+    fn push(&self, ref_name: &str) -> Result<(), Error> {
+        let config = Arc::new(self.git_repo.config()?);
+        let remote_entries = config.multivar("snapshot.remote", None)?;
+        let remote_entries = remote_entries.filter_map(|entry| {
+            entry
+                .ok()
+                .and_then(|entry| entry.value().map(|e| e.to_owned()))
+        });
+
+        for remote in remote_entries {
+            let mut remote = self.git_repo.find_remote(&remote)?;
+            let config = config.clone();
             let mut callbacks = RemoteCallbacks::new();
-            let mut config = self.git_repo.config()?;
-            // if let Ok(p) = git2::Config::find_global() {
-            //     config.add_file(&p, ConfigLevel::Global, true)?;
-            // }
-            // if let Ok(p) = git2::Config::find_system() {
-            //     config.add_file(&p, ConfigLevel::System, true)?;
-            // }
-            for entry in &config.entries(None).unwrap() {
-                let entry = entry.unwrap();
-                println!("{} => {}", entry.name().unwrap(), entry.value().unwrap());
-            }
             callbacks.credentials(move |url, username, allowed_types| {
-                println!("{:?}", allowed_types);
-                Cred::credential_helper(&config, url, username)
+                if allowed_types.is_user_pass_plaintext() {
+                    if let Ok(cred) = Cred::credential_helper(&config, url, username) {
+                        return Ok(cred);
+                    }
+                }
+                if allowed_types.is_ssh_key() {
+                    if let Some(username) = username {
+                        if let Ok(cred) = Cred::ssh_key_from_agent(username) {
+                            return Ok(cred);
+                        }
+                    }
+                }
+                Err(git2::Error::new(
+                    git2::ErrorCode::Auth,
+                    git2::ErrorClass::Callback,
+                    "unable to authenticate",
+                ))
             });
             let mut opts = PushOptions::new();
             opts.remote_callbacks(callbacks);
-            remote.push(
-                &[format!("{}:{}", snapshot_ref, snapshot_ref)],
-                Some(&mut opts),
-            )?;
+            remote.push(&[format!("{}:{}", ref_name, ref_name)], Some(&mut opts))?;
         }
         Ok(())
     }
