5e1cfb1fb792424550d0c131cfcf7643bd411225
Ryl <rylio@users.noreply.github.com>
2022-05-30 18:03:31 -1000

    Updated dependency version specifiers

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,3 @@ mod repo;
 mod util;
 pub use error::*;
 pub use repo::*;
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
