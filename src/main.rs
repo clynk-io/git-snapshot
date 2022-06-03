dbeffc46aadb35619ae36537147fe1bfcbabdc09
Ryl <rylio@users.noreply.github.com>
2022-06-02 16:01:14 -1000

    Snapshot

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+use git_snapshot::watcher::WatchConfig;
 use git_snapshot::Repo;
+use serde_json::to_writer_pretty;
 use std::env::current_dir;
+use std::fs::OpenOptions;
 use structopt::StructOpt;
+
 fn main() {
     let cwd = current_dir().unwrap();
     let repo = Repo::from_path(cwd).unwrap();
     repo.snapshot().unwrap();
+    let f = OpenOptions::new().create(true).open("config.json").unwrap();
+    to_writer_pretty(f, &WatchConfig::default()).unwrap();
 }
--- src/watcher.rs
+++ src/watcher.rs
@@ -1,13 +1,32 @@
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
+use time::Duration;
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(tag = "mode", content = "modeConfig")]
+pub enum WatchMode {
+    Poll { period: Duration },
+    Event,
+}
+
+impl Default for WatchMode {
+    fn default() -> Self {
+        Self::Event
+    }
+}
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct RepoConfig {
     path: PathBuf,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct WatchConfig {}
+#[derive(Debug, Default, Deserialize, Serialize)]
+pub struct WatchConfig {
+    #[serde(default)]
+    repos: Vec<RepoConfig>,
+    #[serde(default)]
+    mode: WatchMode,
+}
 
 pub struct Watcher {
     config_path: PathBuf,
