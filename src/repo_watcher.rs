8d98a34713653b39dbf01b1188ebc5ef190a94d5
Ryl <rylio@users.noreply.github.com>
2022-06-08 01:01:00 -0700

    Snapshot

--- src/repo_watcher.rs
+++ src/repo_watcher.rs
@@ -54,14 +54,15 @@ impl RepoWatcher {
 
         let watcher = Self::watcher(config, debounce_timestamps.clone())?;
         let watcher = Arc::new(Mutex::new(watcher));
+        let watcher_clone = watcher.clone();
         watcher.lock().unwrap().watch_path(
             config_path,
             Box::new(move |path: PathBuf, handler_path: PathBuf| {
                 let config = Self::open_config(&path);
-                let watcher = watcher.clone();
+                let watcher = watcher_clone.clone();
                 if let Ok(config) = config {
                     let w = Self::watcher(config, debounce_timestamps.clone()).unwrap();
-                    let mut watcher = watcher.lock().unwrap();
+                    let mut watcher = watcher.clone().lock().unwrap();
                     *watcher = w;
                 }
             }),
