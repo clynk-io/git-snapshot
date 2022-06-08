26d8510fffa29c120a22a5069e7b5200fa0df31b
Ryl <rylio@users.noreply.github.com>
2022-06-08 00:33:14 -0700

    Snapshot

--- src/repo_watcher.rs
+++ src/repo_watcher.rs
@@ -65,7 +65,7 @@ impl RepoWatcher {
                     return;
                 }
                 if let Ok(repo) = Repo::from_path(&path) {
-                    if !repo.is_ignored(rel).unwrap() {
+                    if !repo.is_ignored(rel).unwrap_or(false) {
                         println!("Event: {}", &Path::new("/").join(rel).to_str().unwrap());
                         repo.snapshot();
                         println!("Took snapshot")
