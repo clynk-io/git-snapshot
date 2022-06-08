cd78f89cc243936219a2fd8f2e461d2687284826
Ryl <rylio@users.noreply.github.com>
2022-06-08 00:31:31 -0700

    Snapshot

--- src/repo.rs
+++ src/repo.rs
@@ -210,8 +210,7 @@ impl Repo {
     }
 
     pub fn is_ignored(&self, path: &Path) -> Result<bool, Error> {
-        self.git_repo
-            .add_ignore_rule(self.git_repo.path().join(Path::new(".gitignore")))?;
+        self.git_repo.add_ignore_rule("/target")?;
         Ok(self.git_repo.is_path_ignored(path)?)
     }
 }
